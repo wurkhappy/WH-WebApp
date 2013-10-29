f28d8b19c362dcba9ed561ae408693b6e42c3ef2
Matt Parker <mdparker89@gmail.com>
2013-10-28 22:48:45 -0400

    revert back to http

--- main.go
+++ main.go
@@ -33,7 +33,7 @@ func main() {
 	http.Handle("/_img/", http.StripPrefix("/_img/", http.FileServer(http.Dir("www/img"))))
 	http.Handle("/js/", http.StripPrefix("/js/", http.FileServer(http.Dir("www/js"))))
 	http.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("www/css"))))
-	err := http.ListenAndServeTLS(":4000", "cert.pem", "key.pem", nil)
+	err := http.ListenAndServe(":4000", nil)
 	if err != nil {
 		log.Fatal(err)
 	}
