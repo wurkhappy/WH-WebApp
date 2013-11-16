9827e9d1847d2b867203d47be46f308d0ec753b9
Marcus Ellison <marcus@wurkhappy.com>
2013-11-16 13:33:51 -0500

    removed the rest of clauses from the frontend

--- www/js/app/routers/agreement_router.js
+++ www/js/app/routers/agreement_router.js
@@ -5,7 +5,7 @@
  define(['backbone', 'models/agreement', 'views/agreement/layout_manager',
   'views/agreement/payments_read_view', 'views/agreement/agreement_history_view', 'views/agreement/user_view',
   'views/agreement/edit/user_edit_view', 'views/agreement/edit/header_edit_view', 'views/agreement/edit/payments_edit_view',
-  'views/agreement/read/header_view', 'views/agreement/discussion_view','models/user', 'views/agreement/clauses_view', 'views/agreement/progress_bar_view'],
+  'views/agreement/read/header_view', 'views/agreement/discussion_view','models/user', 'views/agreement/progress_bar_view'],
 
   function (Backbone, AgreementModel, LayoutView, PaymentsReadView, AgrmntHistoryView, UserView,
     UserEditView, HeaderEditView, PaymentEditView, HeaderView, DiscussionView, UserModel, ClausesView, ProgressBarView) { 
@@ -35,7 +35,6 @@
         this.layout.agreementHistory.show(new AgrmntHistoryView({model: this.model}));
         this.layout.profile.show(new UserView());
         this.layout.header.show(new HeaderView({model: this.model, user: this.user, otherUser: this.otherUser}));
-        this.layout.clauses.show(new ClausesView({collection: _.clone(this.model.get("clauses")), user: this.user, otherUser: this.otherUser}));
         this.layout.discussion.show(new DiscussionView({model: this.model, user: this.user, otherUser: this.otherUser}));
 
         if (this.model.sample) this.sample();
@@ -43,7 +42,6 @@
       editAgreement :function(){
         this.layout.header.show(new HeaderEditView({model: this.model, user:this.user}));
         this.layout.paymentSchedule.show(new PaymentEditView({model: this.model}));
-        this.layout.clauses.show(new ClausesView({collection: _.clone(this.model.get("clauses")), user: this.user, otherUser: this.otherUser}));
         this.layout.agreementHistory.show(new AgrmntHistoryView({model: this.model}));
         this.layout.profile.show(new UserEditView({model: this.model}));
         this.layout.discussion.show(new DiscussionView({model: this.model, user: this.user, otherUser: this.otherUser}));
