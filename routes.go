d0646f5633c08ee861939830228668ff9d760223
Marcus Ellison <marcus@wurkhappy.com>
2013-12-30 12:15:55 -0500

    fixed notifcation showing up in about page

--- routes.go
+++ routes.go
@@ -70,7 +70,6 @@ func home(w http.ResponseWriter, req *http.Request) {
 
 func about(w http.ResponseWriter, req *http.Request) {
 	m := map[string]interface{}{
-		"appName":    "mainhome",
 		"production": handlers.Production,
 		"JSversion":  handlers.JSversion,
 		"CSSversion": handlers.CSSversion,
