2e2448dba09938f1e82e4863ad6838e7eb8f03e3
Marcus Ellison <marcus@wurkhappy.com>
2013-12-30 15:15:41 -0500

    added ckeditor to summary

--- www/js/app/views/create_agreement/proposal_view.js
+++ www/js/app/views/create_agreement/proposal_view.js
@@ -2,9 +2,9 @@
  * Scope of Work - Create Agreement View.
  */
 
- define(['backbone', 'handlebars', 'underscore', 'moment', 'parsley', 'hbs!templates/create_agreement/proposal_tpl'],
+ define(['backbone', 'handlebars', 'underscore', 'moment', 'parsley', 'ckeditor', 'ckadapter', 'hbs!templates/create_agreement/proposal_tpl'],
 
-  function (Backbone, Handlebars, _, moment, parsley, scopeTemplate) {
+  function (Backbone, Handlebars, _, moment, parsley, CKEDITOR, ckadapter, scopeTemplate) {
 
     'use strict';
 
@@ -35,6 +35,20 @@
       },
       onRender: function(){
         if(this.userID === this.model.get("clientID")) this.$('input[name=role][value=clientID]').prop("checked",true);
+
+        CKEDITOR.basePath = 'https://d3kq8dzp7eezz0.cloudfront.net/css/ckeditor/';
+        CKEDITOR.replace('message_editor', {
+          toolbar: [
+          {items: ['Bold','-', 'Italic', '-', 'Underline']}
+          ],
+          disableNativeSpellChecker: false,
+          language: 'https://d3kq8dzp7eezz0.cloudfront.net/css-1/en.js',
+          skin: 'wurkhappy,https://d3kq8dzp7eezz0.cloudfront.net/css-1/wurkhappy/',
+          customConfig : 'https://d3kq8dzp7eezz0.cloudfront.net/css-1/config.js'
+        });
+        CKEDITOR.config.contentsCss = 'https://d3kq8dzp7eezz0.cloudfront.net/css-1/contents.css' ;
+        CKEDITOR.config.stylesSet = 'my_styles:https://d3kq8dzp7eezz0.cloudfront.net/css-1/styles.js';
+
       },
       updateField: function(event){
         this.model.set(event.target.name, event.target.value);
@@ -58,6 +72,11 @@
         this.saveAndContinue();
       },
       saveAndContinue:_.debounce(function(event){
+
+        var editor = CKEDITOR.instances.message_editor;
+        var html = editor.getData();
+
+        this.model.set("proposedServices", html);
 
         $( '.proposal_form' ).parsley( 'validate' );
         var isValid = $( '.proposal_form' ).parsley( 'isValid' );
