fddf84b74b95970dca0b34a2c726ec38ecb1d7e7
Marcus Ellison <marcus@wurkhappy.com>
2014-01-03 18:13:42 -0500

    fixed scrolling issue with legal and privacy pages

--- www/js/app/views/legal/terms.js
+++ www/js/app/views/legal/terms.js
@@ -3,9 +3,9 @@
  * Terms View.
  */
 
- define(['backbone', 'handlebars', 'jquery', 'hbs!templates/legal/terms'],
+ define(['backbone', 'handlebars', 'jquery', 'underscore', 'hbs!templates/legal/terms'],
 
-  function (Backbone, Handlebars, $, termsTemplate) {
+  function (Backbone, Handlebars, $, _, termsTemplate) {
 
     'use strict';
 
@@ -15,6 +15,7 @@
 
       initialize:  function() {
         this.render();
+        $(window).scroll(this.updatePosition);
       },
 
       render: function () {
@@ -26,21 +27,23 @@
           "click .faqnav a" : "toggleSection"
       },
 
+      updatePosition: function() {
+        if ( $(window).scrollTop() + $(window).height() > $(document).height() - 200) {
+          $(".faqnav").addClass('fixed_bottom');
+        } else{
+          $(".faqnav").removeClass('fixed_bottom');
+        }
+      },
+
       toggleSection: function(event) {
         event.preventDefault();
-
         var $section = $(event.target);
-
         var sectionHash = event.target.hash;
-
         $('.faqnav li').removeClass('current');
-
         $section.closest('li').addClass('current');
-
         $('.section').hide();
-
         $(sectionHash).fadeIn();
-      }
+      },
 
     });
 
