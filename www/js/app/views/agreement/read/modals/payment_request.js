519e517616f0ca30ab4e98e0343d56adc75914ba
Matt Parker <mdparker89@gmail.com>
2014-01-14 03:44:20 -0500

    update wurk happy fee

--- www/js/app/views/agreement/read/modals/payment_request.js
+++ www/js/app/views/agreement/read/modals/payment_request.js
@@ -1,158 +1,163 @@
-
 define(['backbone', 'handlebars', 'toastr', 'hbs!templates/agreement/pay_request_tpl',
-  'hbs!templates/agreement/pay_request_methods_tpl', 'hbs!templates/agreement/pay_request_breakout', 'models/payment'],
+        'hbs!templates/agreement/pay_request_methods_tpl', 'hbs!templates/agreement/pay_request_breakout', 'models/payment'
+    ],
 
-  function (Backbone, Handlebars, toastr, payRequestTemplate, paymentMethodsTpl, paymentBreakoutTpl, PaymentModel) {
+    function(Backbone, Handlebars, toastr, payRequestTemplate, paymentMethodsTpl, paymentBreakoutTpl, PaymentModel) {
 
-    'use strict';
-    Handlebars.registerHelper('last_four_digits', function(number) {
-      if (!number) return;
-      return number.slice(-4);
-    });
-    var PaymentMethods = Backbone.View.extend({
+        'use strict';
+        Handlebars.registerHelper('last_four_digits', function(number) {
+            if (!number) return;
+            return number.slice(-4);
+        });
+        var PaymentMethods = Backbone.View.extend({
 
-      template: paymentMethodsTpl,
-      initialize: function(options){
-        this.bankAccounts = options.bankAccounts;
-        this.listenTo(this.bankAccounts, 'add', this.render);
-        this.render();
-      },
-      render:function(){
-        this.$el.html(this.template({bankAccounts: this.bankAccounts.toJSON()}));
-        return this;
-      }
-    });
+            template: paymentMethodsTpl,
+            initialize: function(options) {
+                this.bankAccounts = options.bankAccounts;
+                this.listenTo(this.bankAccounts, 'add', this.render);
+                this.render();
+            },
+            render: function() {
+                this.$el.html(this.template({
+                    bankAccounts: this.bankAccounts.toJSON()
+                }));
+                return this;
+            }
+        });
 
-    var PaymentRequestModal = Backbone.View.extend({
+        var PaymentRequestModal = Backbone.View.extend({
 
-      template: payRequestTemplate,
-      breakoutTpl : paymentBreakoutTpl,
+            template: payRequestTemplate,
+            breakoutTpl: paymentBreakoutTpl,
 
-      events: {
-        "click #pay-button": "requestPayment",
-        "change #milestoneToPay":"updateMilestone",
-        "click #show_fee_detail": "showFeeDetail"
-      },
-      initialize:function(options){
-        this.payment = new PaymentModel();
-        this.payment.agreementVersionID = options.payments.agreementVersionID;
-        this.payment.agreementID = options.payments.agreementID;
-        this.payments = options.payments;
+            events: {
+                "click #pay-button": "requestPayment",
+                "change #milestoneToPay": "updateMilestone",
+                "click #show_fee_detail": "showFeeDetail"
+            },
+            initialize: function(options) {
+                this.payment = new PaymentModel();
+                this.payment.agreementVersionID = options.payments.agreementVersionID;
+                this.payment.agreementID = options.payments.agreementID;
+                this.payments = options.payments;
 
-        this.bankAccounts = options.bankAccounts;
-        this.bankAccounts.fetch();
-        this.paymentMethodsView = new PaymentMethods({bankAccounts: this.bankAccounts});
-        this.acceptsBankTransfer = options.acceptsBankTransfer;
-        this.acceptsCreditCard = options.acceptsCreditCard;
-        if (this.acceptsCreditCard && this.acceptsBankTransfer) {
-          this.allPaymentMethods = true;
-        }
-        this.render();
-      },
+                this.bankAccounts = options.bankAccounts;
+                this.bankAccounts.fetch();
+                this.paymentMethodsView = new PaymentMethods({
+                    bankAccounts: this.bankAccounts
+                });
+                this.acceptsBankTransfer = options.acceptsBankTransfer;
+                this.acceptsCreditCard = options.acceptsCreditCard;
+                if (this.acceptsCreditCard && this.acceptsBankTransfer) {
+                    this.allPaymentMethods = true;
+                }
+                this.render();
+            },
 
-      render:function(event){
-
-
-        this.$el.html(this.template(_.extend({
-          model: this.model.toJSON(),
-          payments: this.collection.toJSON(),
-          acceptsCreditCard: this.acceptsCreditCard,
-          acceptsBankTransfer: this.acceptsBankTransfer,
-          allPaymentMethods: this.allPaymentMethods
-        }, this.calculatePayment())
-        ));
-
-        this.$('header').html(this.paymentMethodsView.$el);
-      },
-      calculatePayment: function(){
-        var milestonePayment = this.model.get("amountDue");
-        var wurkHappyFee = this.wurkHappyFee(milestonePayment);
-        var bankTransferFee = (milestonePayment * .01) + .55;
-        if (bankTransferFee > 5) {
-          bankTransferFee = 5;
-        }
-        var creditCardFee = (milestonePayment * .029) +.55;
-        var processingFee = (this.acceptsCreditCard === true)? creditCardFee: bankTransferFee;
-
-        if (!this.acceptsCreditCard) {
-         var feeTotal = bankTransferFee+wurkHappyFee;
-       } else {
-        var feeTotal = creditCardFee+wurkHappyFee;
-      }
-      var amountTotal = milestonePayment - feeTotal;
+            render: function(event) {
 
 
-      return {
-        milestonePayment: milestonePayment.toFixed(2),
-        wurkHappyFee: wurkHappyFee.toFixed(2),
-        bankTransferFee: bankTransferFee.toFixed(2),
-        creditCardFee: creditCardFee.toFixed(2),
-        processingFee: processingFee.toFixed(2),
-        feeTotal: feeTotal.toFixed(2),
-        amountTotal: amountTotal.toFixed(2)
-      }
-    },
+                this.$el.html(this.template(_.extend({
+                    model: this.model.toJSON(),
+                    payments: this.collection.toJSON(),
+                    acceptsCreditCard: this.acceptsCreditCard,
+                    acceptsBankTransfer: this.acceptsBankTransfer,
+                    allPaymentMethods: this.allPaymentMethods
+                }, this.calculatePayment())));
 
-    wurkHappyFee: function(amount) {
-      if (amount >= 1000) {
-        return 50;
-      } else if (amount >= 500) {
-        return 25;
-      } else if (amount >= 100) {
-        return 15;
-      } else {
-        return 5;
-      }
-    },
-    updateMilestone: function(event){
-      var id = event.target.value;
-      this.model = this.collection.get(id);
+                this.$('header').html(this.paymentMethodsView.$el);
+            },
+            calculatePayment: function() {
+                var milestonePayment = this.model.get("amountDue");
+                var wurkHappyFee = this.wurkHappyFee(milestonePayment);
+                var bankTransferFee = (milestonePayment * .01) + .55;
+                if (bankTransferFee > 5) {
+                    bankTransferFee = 5;
+                }
+                var creditCardFee = (milestonePayment * .029) + .55;
+                var processingFee = (this.acceptsCreditCard === true) ? creditCardFee : bankTransferFee;
 
-      this.$('#paymentBreakout').html(this.breakoutTpl(_.extend({
-        acceptsCreditCard: this.acceptsCreditCard,
-        acceptsBankTransfer: this.acceptsBankTransfer,
-        allPaymentMethods: this.allPaymentMethods
-      }, this.calculatePayment())
-      ));
-    },
-    requestPayment: function (event) {
+                if (!this.acceptsCreditCard) {
+                    var feeTotal = bankTransferFee + wurkHappyFee;
+                } else {
+                    var feeTotal = creditCardFee + wurkHappyFee;
+                }
+                var amountTotal = milestonePayment - feeTotal;
 
-      if (this.bankAccounts.length < 1) {
-        toastr.error('Please add a bank account to receive payment');
-        return;
-      }
 
-      var fadeOutModal = function () {
-        $('#overlay').fadeOut('fast');
-      };
+                return {
+                    milestonePayment: milestonePayment.toFixed(2),
+                    wurkHappyFee: wurkHappyFee.toFixed(2),
+                    bankTransferFee: bankTransferFee.toFixed(2),
+                    creditCardFee: creditCardFee.toFixed(2),
+                    processingFee: processingFee.toFixed(2),
+                    feeTotal: feeTotal.toFixed(2),
+                    amountTotal: amountTotal.toFixed(2)
+                }
+            },
 
-      var fadeInNotification = function () {
-        toastr.success('Payment requested and email sent');
-      };
+            wurkHappyFee: function(amount) {
+                var fee = amount * 0.01;
+                if (fee > 10) {
+                    fee = 10;
+                }
+                return fee;
+            },
+            updateMilestone: function(event) {
+                var id = event.target.value;
+                this.model = this.collection.get(id);
 
-      var triggerNotification = _.debounce(fadeInNotification, 300);
+                this.$('#paymentBreakout').html(this.breakoutTpl(_.extend({
+                    acceptsCreditCard: this.acceptsCreditCard,
+                    acceptsBankTransfer: this.acceptsBankTransfer,
+                    allPaymentMethods: this.allPaymentMethods
+                }, this.calculatePayment())));
+            },
+            requestPayment: function(event) {
 
-      this.payment.get("paymentItems").add({workItemID: this.model.id, amount: this.model.get("amountDue")});
-      this.payments.add(this.payment, {silent:true})
-      var creditSource = this.$(".select_bank_account:checked").attr("value") || '';
-      this.payment.submit({creditSourceURI: creditSource}, _.bind(function(response){
-        fadeOutModal();
-        triggerNotification();
-        this.payments.trigger('add');
-        this.trigger("paymentRequested");
-        this.trigger('hide');
-      }, this));
-    },
+                if (this.bankAccounts.length < 1) {
+                    toastr.error('Please add a bank account to receive payment');
+                    return;
+                }
 
-    showFeeDetail: function(event) {
-      event.preventDefault();
-      event.stopPropagation();
-      $('.fee_detail_container').slideToggle('slow');
+                var fadeOutModal = function() {
+                    $('#overlay').fadeOut('fast');
+                };
+
+                var fadeInNotification = function() {
+                    toastr.success('Payment requested and email sent');
+                };
+
+                var triggerNotification = _.debounce(fadeInNotification, 300);
+
+                this.payment.get("paymentItems").add({
+                    workItemID: this.model.id,
+                    amount: this.model.get("amountDue")
+                });
+                this.payments.add(this.payment, {
+                    silent: true
+                })
+                var creditSource = this.$(".select_bank_account:checked").attr("value") || '';
+                this.payment.submit({
+                    creditSourceURI: creditSource
+                }, _.bind(function(response) {
+                    fadeOutModal();
+                    triggerNotification();
+                    this.payments.trigger('add');
+                    this.trigger("paymentRequested");
+                    this.trigger('hide');
+                }, this));
+            },
+
+            showFeeDetail: function(event) {
+                event.preventDefault();
+                event.stopPropagation();
+                $('.fee_detail_container').slideToggle('slow');
+            }
+
+        });
+
+        return PaymentRequestModal;
+
     }
-
-  });
-
-return PaymentRequestModal;
-
-}
 );
