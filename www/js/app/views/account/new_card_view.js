494e13b4cdd95c81ac99ce4ac955a1cc328dc65c
Matt Parker <mdparker89@gmail.com>
2014-01-16 22:29:07 -0500

    fix credit card bug

--- www/js/app/views/account/new_card_view.js
+++ www/js/app/views/account/new_card_view.js
@@ -2,61 +2,66 @@
  * Credit Card Account View.
  */
 
- define(['jquery', 'backbone', 'handlebars', 'toastr', 'hbs!templates/account/new_card_tpl'],
+define(['jquery', 'backbone', 'handlebars', 'toastr', 'hbs!templates/account/new_card_tpl'],
 
-  function ($, Backbone, Handlebars, toastr, Template) {
+    function($, Backbone, Handlebars, toastr, Template) {
 
-    'use strict';
+        'use strict';
 
-    var NewCardView = Backbone.View.extend({
+        var NewCardView = Backbone.View.extend({
 
-      template: Template,
-      events:{
-        "blur input":"updateFields",
-        "click #save-button":"debounceSaveCard",
-        "change select":"updateFields"
-      },
+            template: Template,
+            events: {
+                "blur input": "updateFields",
+                "click #save-button": "debounceSaveCard",
+                "change select": "updateFields"
+            },
 
-      initialize: function (options) {
-        this.card = {"expiration_month":1, "expiration_year":2013};
-        this.user = options.user;
-        this.render();
-      },
+            initialize: function(options) {
+                this.card = {
+                    "expiration_month": 1,
+                    "expiration_year": 2014
+                };
+                this.user = options.user;
+                this.render();
+            },
 
-      render: function () {
-        this.$el.html(this.template());
-        return this;
-      },
-      updateFields:function(event){
-        this.card[event.target.name] = event.target.value;
-      },
+            render: function() {
+                this.$el.html(this.template());
+                return this;
+            },
+            updateFields: function(event) {
+                this.card[event.target.name] = event.target.value;
+            },
 
-      debounceSaveCard: function(event) {
-        event.preventDefault();
-        event.stopPropagation();
-        this.saveCard();
-      },
-      saveCard:_.debounce(function(event){
-        var that = this;
-        balanced.card.create(this.card, function (response) {
-          if(response.status === 201) {
-            if (response.data.id) {delete response.data.id}
-              var model = new that.user.attributes["cards"].model(response.data);
-            that.user.get("cards").add(model);
-            model.save();
-            $('input').val('');
-            that.trigger('cardSaved');
-            toastr.success('Credit Card Saved!');
+            debounceSaveCard: function(event) {
+                event.preventDefault();
+                event.stopPropagation();
+                this.saveCard();
+            },
+            saveCard: _.debounce(function(event) {
+                var that = this;
+                balanced.card.create(this.card, function(response) {
+                    if (response.status === 201) {
+                        if (response.data.id) {
+                            delete response.data.id
+                        }
+                        var model = new that.user.attributes["cards"].model(response.data);
+                        that.user.get("cards").add(model);
+                        model.save();
+                        $('input').val('');
+                        that.trigger('cardSaved');
+                        toastr.success('Credit Card Saved!');
 
-          } else {
-            console.log(response);
-          }
+                    } else {
+                        console.log(response);
+                    }
+                });
+            }, 500, true)
+
         });
-      }, 500, true)
 
-    });
+        return NewCardView;
 
-return NewCardView;
-
-}
+    }
 );
