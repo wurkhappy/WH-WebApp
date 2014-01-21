2f01675bdf2ae78fec6bf6cc0937f35ddb8b4904
MattParker89 <mdparker89@gmail.com>
2014-01-20 22:11:08 -0500

    add user verification info to create agreement

--- handlers/utilities.go
+++ handlers/utilities.go
@@ -8,6 +8,7 @@ import (
 	"github.com/wurkhappy/mdp"
 	"html/template"
 	"net/http"
+	"time"
 )
 
 var UserService string = config.UserService
@@ -54,6 +55,26 @@ type ServiceResp struct {
 
 func sendServiceRequest(method, service, path string, body []byte) (response []byte, statusCode int) {
 	client := mdp.NewClient(config.MDPBroker, false)
+	defer client.Close()
+	m := map[string]interface{}{
+		"Method": method,
+		"Path":   path,
+		"Body":   body,
+	}
+	req, _ := json.Marshal(m)
+	request := [][]byte{req}
+	reply := client.Send([]byte(service), request)
+	if len(reply) == 0 {
+		return nil, 404
+	}
+	resp := new(ServiceResp)
+	json.Unmarshal(reply[0], &resp)
+	return resp.Body, int(resp.StatusCode)
+}
+
+func sendServiceRequestWithTimeout(method, service, path string, body []byte, timeout int64) (response []byte, statusCode int) {
+	client := mdp.NewClient(config.MDPBroker, false)
+	client.Timeout = time.Duration(timeout * int64(time.Millisecond))
 	defer client.Close()
 	m := map[string]interface{}{
 		"Method": method,
