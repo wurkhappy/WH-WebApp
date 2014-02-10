9b4cd4a972098d3e47b4f21d17f4a3091ec52430
Marcus Ellison <marcus@wurkhappy.com>
2014-02-10 11:18:09 -0500

    ?

--- www/js/app/views/home/agreement_view.js
+++ www/js/app/views/home/agreement_view.js
@@ -20,6 +20,7 @@
         var otherUserID = (this.userIsClient) ? this.model.get("freelancerID") : this.model.get("clientID");
         this.otherUser = options.otherUsers.get(otherUserID);
         this.percentComplete = this.model.percentComplete();
+        console.log(this.percentComplete);
       },
 
       render: function () {
