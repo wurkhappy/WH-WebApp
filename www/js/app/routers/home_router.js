9075e8a3a90defa1804f4d8a7f016fbf28652578
Matt Parker <mdparker89@gmail.com>
2014-04-09 11:16:19 -0400

    remove user verification requirement in home page

--- www/js/app/routers/home_router.js
+++ www/js/app/routers/home_router.js
@@ -2,88 +2,78 @@
  * Router. Initializes the root-level View(s), and calls the render() method on Sub-View(s).
  */
 
- define(['backbone', 'flying-focus', 'toastr', 'collections/agreements', 'collections/users', 'models/user', 'views/home/home_section_view', 'moment'],
+define(['backbone', 'flying-focus', 'toastr', 'collections/agreements', 'collections/users', 'models/user', 'views/home/home_section_view', 'moment'],
 
-  function (Backbone, FlyingFocus, toastr, AgreementCollection, UserCollection, UserModel, SectionView, moment) {
+    function(Backbone, FlyingFocus, toastr, AgreementCollection, UserCollection, UserModel, SectionView, moment) {
 
-    'use strict';
+        'use strict';
 
-    var HomeRouter = Backbone.Router.extend({
+        var HomeRouter = Backbone.Router.extend({
 
-      routes: {
-        '': 'AllAgreements',
-        'Freelancer': 'FreelancerAgreements',
-        'Client': 'ClientAgreements',
-      },
+            routes: {
+                '': 'AllAgreements',
+                'Freelancer': 'FreelancerAgreements',
+                'Client': 'ClientAgreements',
+            },
 
-      initialize: function () {
-        this.collection = new AgreementCollection(window.agreements);
-        this.otherUsers = new UserCollection(window.otherUsers);
-        this.currentUser = new UserModel(window.currentUser);
-        FlyingFocus();
-        this.isVerified = this.currentUser.get("isVerified");
-        if (!this.isVerified) {
-          $("#create_agreement").removeAttr('href');
-          toastr.info("Please verify your email");
-          this.notVerified();
-        } else if(window.production) {
-          $("#create_agreement").click( function(event) {
-            analytics.track('Clicked Create Agreement');   
-          });
+            initialize: function() {
+                this.collection = new AgreementCollection(window.agreements);
+                this.otherUsers = new UserCollection(window.otherUsers);
+                this.currentUser = new UserModel(window.currentUser);
+                FlyingFocus();
+                this.isVerified = this.currentUser.get("isVerified");
+                if (window.production) {
+                    $("#create_agreement").click(function(event) {
+                        analytics.track('Clicked Create Agreement');
+                    });
+                }
 
-        }
+            },
 
-      },
+            AllAgreements: function() {
+                //need to to views
+                //same view with different title and different models
+                var sortedAgreements = this.collection.sortByStatus();
 
-      AllAgreements: function () {
-        //need to to views
-        //same view with different title and different models
-        var sortedAgreements = this.collection.sortByStatus();
+                if (sortedAgreements.waitingOnRespAgrmnts.length > 0) {
+                    var waitingView = new SectionView({
+                        title: 'Waiting for Response',
+                        collection: sortedAgreements.waitingOnRespAgrmnts,
+                        otherUsers: this.otherUsers,
+                        currentUser: this.currentUser,
+                        el: '#waitingSection'
+                    });
+                }
+                if (sortedAgreements.inProgressAgrmnts.length > 0) {
+                    var progressView = new SectionView({
+                        title: "In Progress",
+                        collection: sortedAgreements.inProgressAgrmnts,
+                        otherUsers: this.otherUsers,
+                        currentUser: this.currentUser,
+                        el: '#progressSection'
+                    });
+                }
+                if (sortedAgreements.draftAgrmnts.length > 0) {
+                    var draftView = new SectionView({
+                        title: "Drafts",
+                        collection: sortedAgreements.draftAgrmnts,
+                        otherUsers: this.otherUsers,
+                        currentUser: this.currentUser,
+                        el: '#draftSection'
+                    });
+                }
+            },
 
-        if (sortedAgreements.waitingOnRespAgrmnts.length > 0) {
-          var waitingView = new SectionView({
-            title: 'Waiting for Response', 
-            collection: sortedAgreements.waitingOnRespAgrmnts,
-            otherUsers: this.otherUsers, 
-            currentUser: this.currentUser, 
-            el:'#waitingSection'
-          });
-        }
-        if (sortedAgreements.inProgressAgrmnts.length > 0) {
-          var progressView = new SectionView({
-            title:"In Progress", 
-            collection: sortedAgreements.inProgressAgrmnts,
-            otherUsers: this.otherUsers,
-            currentUser: this.currentUser, 
-            el:'#progressSection'
-          });
-        }
-        if (sortedAgreements.draftAgrmnts.length > 0) {
-          var draftView = new SectionView({
-            title:"Drafts", 
-            collection: sortedAgreements.draftAgrmnts, 
-            otherUsers: this.otherUsers,
-            currentUser: this.currentUser, 
-            el:'#draftSection'
-          });
-        }
-      },
+            FreelancerAgreements: function() {
 
-      FreelancerAgreements: function () {
+            },
+            ClientAgreements: function() {
 
-      },
-      ClientAgreements: function () {
+            }
 
-      },
-      notVerified: function() {
-        $("#create_agreement").click( function(event) {
-          toastr.info("You must verify your email before you can create an agreement");
         });
-      }
 
-    });
+        return HomeRouter;
 
-return HomeRouter;
-
-}
+    }
 );
--- www/js/app/routers/landing_router.js
+++ www/js/app/routers/landing_router.js
@@ -2,84 +2,90 @@
  * Router. Initializes the root-level View(s), and calls the render() method on Sub-View(s).
  */
 
- define(['backbone', 'parsley', 'flying-focus', 'views/landing/landingview', 'views/landing/loginview',
-  'views/landing/new_account', 'views/landing/forgot_password','views/landing/check_email_view', 'views/landing/form_modal'],
+define(['backbone', 'parsley', 'flying-focus', 'views/landing/landingview', 'views/landing/loginview',
+        'views/landing/new_account', 'views/landing/forgot_password', 'views/landing/check_email_view', 'views/landing/form_modal'
+    ],
 
-  function (Backbone, parsley, FlyingFocus, LandingView, LoginView, NewAccountView, ForgotPasswordView, CheckEmailView, FormModal) {
+    function(Backbone, parsley, FlyingFocus, LandingView, LoginView, NewAccountView, ForgotPasswordView, CheckEmailView, FormModal) {
 
-    'use strict';
+        'use strict';
 
-    var LandingRouter = Backbone.Router.extend({
+        var LandingRouter = Backbone.Router.extend({
 
-      routes: {
-        '': 'index',
-        'login':"login",
-        'new-account':"newAccount",
-        'forgot-password': "forgotPassword",
-        'check-email': "checkEmail"
-      },
+            routes: {
+                '': 'index',
+                'login': "login",
+                'new-account': "newAccount",
+                'forgot-password': "forgotPassword",
+                'check-email': "checkEmail"
+            },
 
-      initialize: function () {
-        this.$mainSection = $(".fade2");
-        FlyingFocus();
-      },
+            initialize: function() {
+                this.$mainSection = $(".fade2");
+                FlyingFocus();
+            },
 
-      index: function () {
-        this.hideForm();
-        if (!this.landingView) this.landingView = new LandingView({router:this, model: this.model});
-        this.$mainSection.html(this.landingView.el);
-      },
-      login: function(){
-        var view = new LoginView();
-        this.showForm(view);
-      },
-      newAccount: function(){
-        var view = new NewAccountView();
-        this.listenTo(view, 'saveSuccess', this.createAccountSuccess);
-        this.showForm(view);
-      },
-      forgotPassword: function(){
-        var view = new ForgotPasswordView();
-        this.showForm(view);
-      },
-      checkEmail: function(){
-        var view = new CheckEmailView();
-        this.showForm(view);
-      },
-      showForm: function(view){
-        this.formView = new FormModal({childView: view});     
-        this.$mainSection.html(this.formView.el);
-        $('#logindiv').fadeIn('slow');
-        $('#target').focus();
-        $('#login_form').parsley();
-        $('#log_in').hide();
-        $('#log_out').show();
-        $('#create_account').hide();
-        $('.copyright').addClass('copyright_login');
-        $('.landing_footer').addClass('landing_footer_login');
-      },
-      hideForm: function(){
-        if (!this.formView) return;
-        $('#create_form').parsley();
-        this.$mainSection.hide();
-        this.$mainSection.fadeIn('fast');
-        $('.copyright').removeClass('copyright_login');
-        $('.landing_footer').removeClass('landing_footer_login');
+            index: function() {
+                this.hideForm();
+                if (!this.landingView) this.landingView = new LandingView({
+                    router: this,
+                    model: this.model
+                });
+                this.$mainSection.html(this.landingView.el);
+            },
+            login: function() {
+                var view = new LoginView();
+                this.showForm(view);
+            },
+            newAccount: function() {
+                var view = new NewAccountView();
+                this.listenTo(view, 'saveSuccess', this.createAccountSuccess);
+                this.showForm(view);
+            },
+            forgotPassword: function() {
+                var view = new ForgotPasswordView();
+                this.showForm(view);
+            },
+            checkEmail: function() {
+                var view = new CheckEmailView();
+                this.showForm(view);
+            },
+            showForm: function(view) {
+                this.formView = new FormModal({
+                    childView: view
+                });
+                this.$mainSection.html(this.formView.el);
+                $('#logindiv').fadeIn('slow');
+                $('#target').focus();
+                $('#login_form').parsley();
+                $('#log_in').hide();
+                $('#log_out').show();
+                $('#create_account').hide();
+                $('.copyright').addClass('copyright_login');
+                $('.landing_footer').addClass('landing_footer_login');
+            },
+            hideForm: function() {
+                if (!this.formView) return;
+                $('#create_form').parsley();
+                this.$mainSection.hide();
+                this.$mainSection.fadeIn('fast');
+                $('.copyright').removeClass('copyright_login');
+                $('.landing_footer').removeClass('landing_footer_login');
 
-        $('#log_out').hide();
-        $('#log_in').show();
-        $('#create_account').show();
-        $('#logindiv').fadeOut('fast');
-        this.formView.remove();
-        this.formView = null;
-      },
-      createAccountSuccess: function(){
-        window.location = "/#check-email";
-      }
+                $('#log_out').hide();
+                $('#log_in').show();
+                $('#create_account').show();
+                $('#logindiv').fadeOut('fast');
+                this.formView.remove();
+                this.formView = null;
+            },
+            createAccountSuccess: function() {
+                window.location = "/home";
+            }
 
-    });
+        });
 
-return LandingRouter;
+        return LandingRouter;
 
-}
+    }
 );
