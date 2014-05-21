e0cf7ebfd9c56b70ebd71e8d90dffdbe22e1ac3e
Marcus Ellison <marcus@wurkhappy.com>
2014-05-21 16:04:41 -0400

    added signup to welcome message, minor tweaks

--- www/js/app/routers/sample_home_router.js
+++ www/js/app/routers/sample_home_router.js
@@ -2,9 +2,9 @@
  * Router. Initializes the root-level View(s), and calls the render() method on Sub-View(s).
  */
 
-define(['backbone', 'flying-focus', 'toastr', 'collections/agreements', 'collections/users', 'models/user', 'views/sample_home/home_section_view', 'moment', 'views/ui-modules/modal', 'views/sample_home/welcome', 'views/sample_home/introduction_progress_view'],
+define(['backbone', 'flying-focus', 'toastr', 'collections/agreements', 'collections/users', 'models/user', 'views/sample_home/home_section_view', 'moment', 'views/ui-modules/modal', 'views/sample_home/welcome', 'views/sample_home/introduction_progress_view','views/sample_home/signup'],
 
-    function(Backbone, FlyingFocus, toastr, AgreementCollection, UserCollection, UserModel, SectionView, moment, Modal, WelcomeModal, IntroductionProgressView) {
+    function(Backbone, FlyingFocus, toastr, AgreementCollection, UserCollection, UserModel, SectionView, moment, Modal, WelcomeModal, IntroductionProgressView, SignupModal) {
 
         'use strict';
 
@@ -20,12 +20,17 @@ define(['backbone', 'flying-focus', 'toastr', 'collections/agreements', 'collect
                 this.collection = new AgreementCollection(window.agreements);
                 this.otherUsers = new UserCollection(window.otherUsers);
                 this.currentUser = new UserModel(window.currentUser);
+                this.numberOfTimesVisited = window.numberOfTimesVisited;
                 FlyingFocus();
                 if (window.production) {
                     $("#create_agreement").click(function(event) {
-                        analytics.track('Potential Client Clicked Create Agreement');
+                        analytics.track('Potential Client Create Agreement');
                     });
                 }
+                $('a:not(.clickable)').click( function(event) {
+                    event.preventDefault();
+                    toastr.error('Please create an account for full access');
+                });                
             },
 
             AllAgreements: function() {
@@ -62,11 +67,20 @@ define(['backbone', 'flying-focus', 'toastr', 'collections/agreements', 'collect
                 }
 
                 //Display a modal when the page loads
-                var view = new WelcomeModal();
-                this.modal = new Modal({
-                    view: view
-                });
-                this.modal.show();
+                if (this.numberOfTimesVisited < 3) {
+                    var view = new WelcomeModal();
+                    this.modal = new Modal({
+                        view: view
+                    });
+                    this.modal.show();
+                } else {
+                    var view = new SignupModal();
+                    this.modal = new Modal({
+                        view: view
+                    });
+                    this.modal.show();
+                }
+                
 
                 // The progress bar boom!
                 new IntroductionProgressView({
